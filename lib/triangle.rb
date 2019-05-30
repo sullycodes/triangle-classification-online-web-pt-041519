6c434c91d7a44aab4c43eeb56f679d9825d36e9c
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-30 15:15:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,14 +1,15 @@
 class Triangle
 
-  # side1 == side2 && side2 != side3 || side1 != side2 && side2 == side3 || side1 == side3 && side2 != side3
-
   # attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(side1, side2, side3)
+    @sides = []
     @side1 = side1
     @side2 = side2
     @side3 = side3
-    # @sides = << @side1, @side2, @side3 
+    @sides << side1 
+    @sides << side2
+    @sides << side3
   end
 
   def kind 
