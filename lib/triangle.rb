7bfaefc771f94e15f7c85b0ae8a201eb0b0981fb
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-30 14:03:22 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,13 +1,18 @@
 class Triangle
 
-  attr_accessor :equilateral, :isosceles, :scalene
+  # attr_accessor :equilateral, :isosceles, :scalene
   
   def initialize(side1, side2, side3)
-    
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
   end
 
   def kind 
-    
+    if side1 == side2 && side2 == side3
+    :equilateral
+    elsif 
+      
   end
 
 end
