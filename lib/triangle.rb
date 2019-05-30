b36622415ae65116140c10c0db300441a2c5e0d1
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-30 14:22:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -18,7 +18,7 @@ class Triangle
       @side1 != @side2 && @side2 != @side3 && @side1 != @side3
       :scalene
     elsif 
-      @sides.any? 
+      @sides.any? { |e| 
     else
       :isosceles
     end
