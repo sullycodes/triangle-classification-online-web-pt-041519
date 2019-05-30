818eaaab3db4ebd72ed60f692d386fee7b5b58c5
Daniel Sullivan <dansullivan19@gmail.com>
2019-05-30 15:22:42 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,44 @@
 class Triangle
-  # write code here
+
+  # attr_accessor :equilateral, :isosceles, :scalene
+  
+  def initialize(side1, side2, side3)
+    @sides = []
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+    @sides << side1 
+    @sides << side2
+    @sides << side3
+  end
+
+  def kind 
+    if @sides.any? {|e| e == 0 } || 
+        @sides.any? {|e| e < 0 } ||
+        @side1 + @side2 <= @side3 || @side2 + @side3 <= @side1 || @side1 + @side3 <= @side2
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    elsif @side1 == @side2 && @side2 == @side3
+      :equilateral
+    elsif 
+      @side1 != @side2 && @side2 != @side3 && @side1 != @side3
+      :scalene
+    else
+      :isosceles
+    end
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "Triangle is invalid."
+    end
+  end
+
+
 end
+
+
+
+
